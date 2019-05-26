fb2a8d44fce5859493d9fe8c450a30fb5bc146c3
Fiona Tolliday <fionatolliday@hotmail.com>
2019-05-26 10:04:24 +1000

    throwing exception if water over the limit of the tank

--- src/CoffeeMaker.java
+++ src/CoffeeMaker.java
@@ -1,37 +1,35 @@
 public class CoffeeMaker {
 
-Coffee coffee = new Coffee();
-RunCoffeeMachine runMachine = new RunCoffeeMachine();
+private Coffee coffee = new Coffee();
 WaterDispenser water = new WaterDispenser();
 MilkDispenser milk = new MilkDispenser();
 CoffeePowderDispenser coffeePowder = new CoffeePowderDispenser();
 
-   public String whatType() {
-        if (runMachine.readUserInput.equals("flat white")) {
+    public String whatType(String type) {
+        if (type.equals("flat white")) {
 
             return "Making you a flat white.";
         }
-        if (runMachine.readUserInput.equals("latte")) {
+        if (type.equals("latte")) {
             return "Making you a latte.";
         }
-        if (runMachine.readUserInput.equals("black coffee")) {
+        if (type.equals("black coffee")) {
             return "Making you a black coffee.";
         }
         return null;
     }
 
 
-
     public Coffee makeCoffee(String type) {
-        if (coffee.getType() == "black coffee") {
-            water.getWater(40);
+        if (type.equals("black coffee")) {
+            water.getWater(250);
             milk.getMilk(0);
             coffeePowder.getCoffeePowder(15);
-        } else if (coffee.getType() == "flat white") {
+        } else if (type.equals("flat white")) {
             milk.getMilk(10);
             coffeePowder.getCoffeePowder(8);
             water.getWater(30);
-        } else if (coffee.getType() == "latte") {
+        } else if (type.equals("latte")) {
             milk.getMilk(20);
             coffeePowder.getCoffeePowder(8);
             water.getWater(20);
--- src/RunCoffeeMachine.java
+++ src/RunCoffeeMachine.java
@@ -2,27 +2,19 @@ import java.util.Scanner;
 
 public class RunCoffeeMachine {
 
-    static String readUserInput;
-
-
     public static void main(String[] args) {
+
+
         CoffeeMaker coffeeMaker = new CoffeeMaker();
 
         Coffee coffee = new Coffee();
         Scanner userInput = new Scanner(System.in);
 
-
         System.out.println("Please type a coffee you would like: ");
-        readUserInput = userInput.nextLine().toLowerCase();
+        String readUserInput = userInput.nextLine().toLowerCase();
         coffee.setType(readUserInput);
-        coffeeMaker.makeCoffee(readUserInput);
-        System.out.println(coffeeMaker.whatType());
-
-
-
+        System.out.println(coffeeMaker.whatType(readUserInput));
+        System.out.println(coffeeMaker.makeCoffee(readUserInput));
 
     }
-
-
-
 }
